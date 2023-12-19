45e64fcf2f77f083af5da6ca4effc2939fcf3e48
d.zaitsev <d.zaitsev@urancompany.com>
2023-12-19 22:34:04 +0200

    add queries

--- 1_facebook_api/src/controllers/homepageController.js
+++ 1_facebook_api/src/controllers/homepageController.js
@@ -7,23 +7,48 @@
 //         rejectUnauthorized: false
 //     }
 // });
-// client.connect();
 //
+// client.connect();
+
 // let tableName = 'public.messages'
-// client.query(`SELECT * FROM ${tableName}`, (err, res) => {
+// select all data
+// client.query(`SELECT * FROM ${tableName}`, function (err, res) {
 //     if (err) throw err;
 //     for (let row of res.rows) {
 //         console.log(JSON.stringify(row));
 //     }
 //     client.end();
 // });
-// client.query('INSERT INTO ${tableName} (psid, text, media) VALUES ($1, $2, $3);', ['psid-X', 'text_message', 'image_name'], (err, res) => {
+
+// count messages with same psid for sending "Hello <user_name>" if count is zero
+// let psid = 6239245069511534
+// client.query(`SELECT COUNT(*) FROM ${tableName} WHERE psid = '${psid}'`, function (err, res) {
 //     if (err) throw err;
 //     for (let row of res.rows) {
 //         console.log(JSON.stringify(row));
 //     }
 //     client.end();
 // });
+
+// save new message
+// client.query(`INSERT INTO ${tableName} (psid, text, media) VALUES ($1, $2, $3);`, ['psid-1', 'text_message-1', 'image_name-1'], (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+// });
+
+//get last message
+// let psid = 'qazwsx'
+// client.query(`SELECT * FROM ${tableName} WHERE psid = '${psid}' ORDER BY id DESC LIMIT 1`, function (err, res) {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+// });
+
 
 let getHomepage = (req, res) => {
     return res.render("homepage.ejs");
