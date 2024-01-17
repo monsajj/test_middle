620e471b541ab6c74e3224e47e1c319829324b02
d.zaitsev <d.zaitsev@urancompany.com>
2024-01-17 18:02:42 +0200

    little fixes

--- 1_facebook_api/src/controllers/chatBotController.js
+++ 1_facebook_api/src/controllers/chatBotController.js
@@ -108,7 +108,7 @@ const handleMessage = async (sender_psid, received_message) => {
         }
     } else if (received_message.attachments && received_message.attachments.length > 0) {
         // Gets the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
+        const attachment_url = received_message.attachments[0].payload.url;
         response = {
             "attachment": {
                 "type": "template",
